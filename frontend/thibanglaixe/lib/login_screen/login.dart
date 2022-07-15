8297a2a671260f1077adb69fc6b7f8077e5a086c
DESKTOP-F4DDIQ8\ahihi <manhdung5801@gmail.com>
2022-07-15 17:06:44 +0700

    build login screen

---
+++ frontend/thibanglaixe/lib/login_screen/login.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:thibanglaixe/login_screen/components/login_button.dart';
+
+import 'components/email_input_field.dart';
+import 'components/forgot_password.dart';
+import 'components/password_input_field.dart';
+import 'components/title_area.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: [
+            title_area(size: size),
+            SizedBox(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40.0,
+                  vertical: 20.0,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const email_input_field(),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      const password_input_field(),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      forgot_password(size: size),
+                      const SizedBox(
+                        height: 70.0,
+                      ),
+                      login_button(size: size),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/thibanglaixe/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:thibanglaixe/login_screen/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.green,
+        statusBarBrightness: Brightness.dark,
+      ),
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
